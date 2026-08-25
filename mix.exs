ae0869c12f66ea7d084dd7164669c587d76a5a3c
Skyler Blue Spillers <92972770+skylerblue333@users.noreply.github.com>
2026-08-24 21:11:05 -0500

    product: add real Elixir report project

---
+++ mix.exs
@@ -0,0 +1,17 @@
+defmodule SkyReport.MixProject do
+  use Mix.Project
+
+  def project do
+    [
+      app: :sky_report,
+      version: "0.1.0",
+      elixir: "~> 1.17",
+      start_permanent: Mix.env() == :prod,
+      deps: []
+    ]
+  end
+
+  def application do
+    [extra_applications: [:logger]]
+  end
+end
